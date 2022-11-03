589924169b92be10e88f57157c0943f4f262c001
Tiago Jose De Sousa Machado <tde-sous@c1r12s11.42porto.com>
2022-11-03 14:29:23 +0000

    +isalpha + isdigit

--- libft/ft/ft_isdigit.c
+++ libft/ft/ft_isdigit.c
@@ -1,13 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   teste.c                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tde-sous <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tde-sous <tde-sous@42.porto.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/02 18:11:00 by tde-sous          #+#    #+#             */
-/*   Updated: 2022/11/02 18:11:04 by tde-sous         ###   ########.fr       */
+/*   Created: 2022/11/03 14:19:18 by tde-sous          #+#    #+#             */
+/*   Updated: 2022/11/03 14:26:26 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-sd
+int	ft_isdigit(char c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
+
+/*#include <ctype.h>
+#include <stdio.h>
+int main(void)
+{
+	char c = '0';
+	printf("%i\n", isdigit(c));
+	printf("%i", ft_isdigit(c));
+	return 0;		
+}*/
